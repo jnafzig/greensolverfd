7832f4cd7aa86518dddd2949352737a26029022a
Jonathan <jnafzig@purdue.edu>
2017-02-20 16:01:22 -0500

    forgot to add ked_solver to last commit

---
+++ kedsolver_fh.m
@@ -0,0 +1,41 @@
+function [ kedsolver_fh ] = kedsolver_fh(Nelem, dx, RelTol,AbsTol)
+    if nargin<3
+        RelTol = eps;
+    end
+    if nargin<4
+        AbsTol = eps;
+    end
+    
+    shoot = shoot_fh(Nelem,dx);
+    dens = @(E,v,vL,vR) ldos(shoot(E,v,vL,vR));
+    
+    % return function handle
+    kedsolver_fh = @kedsolve;
+
+    function [n,ts] = kedsolve(mu,v,vL,vR)
+        
+        E0 = min(v)-1;
+
+        R = (E0+mu)/2; 
+        A = mu-R; 
+        E = @(theta) R + A*exp(1i*theta);
+        dEdt = @(theta) 1i*A*exp(1i*theta); 
+ 
+        n = integral(@(theta) dens(E(theta),v,vL,vR)*dEdt(theta),0,pi,...
+            'ArrayValued',true,...
+            'RelTol',RelTol,...
+            'AbsTol',AbsTol);
+        n = n+conj(n);
+        
+        e = integral(@(theta) E(theta)*dens(E(theta),v,vL,vR)*dEdt(theta),0,pi,...
+            'ArrayValued',true,...
+            'RelTol',RelTol,...
+            'AbsTol',AbsTol);
+        e = e+conj(e);
+        
+        ts = e - v.*n;
+        
+    end
+        
+end
+
