9b73449d5c6cab02b7eb8e82c82cf75de3b07c27
Jonathan <jnafzig@purdue.edu>
2017-03-06 14:47:23 -0500

    updated aitkins

--- Examples/Aitkens.m
+++ Examples/Aitkens.m
@@ -4,10 +4,10 @@ clear;
 
 ColOrd = get(gca,'ColorOrder');
 
-A = -15;
-B = 15;
+A = -8;
+B = 8;
 
-NelemValues = 6:13;
+NelemValues = 5:13;
 NumVals = numel(NelemValues);
 
 x = cell(NumVals,1);
@@ -17,7 +17,7 @@ Eval1 = zeros(NumVals,1);
 dx = zeros(NumVals,1);
 
 for i = 1:NumVals;
-    Nelem = 2^NelemValues(i);
+    Nelem = 2^NelemValues(i)+1;
     x{i} = linspace(A,B,Nelem)';
     dx(i) = x{i}(2)-x{i}(1);
 
@@ -38,12 +38,17 @@ hold off;
 
 
 dvals0 = diff(Evals0);
-d2vals0 = diff(Evals0,2);
+d2vals0 = diff(dvals0);
 Evals1 = (Evals0(1:end-2)-dvals0(1:end-1).^2./d2vals0);
 
 
+dvals1 = diff(Evals1);
+d2vals1 = diff(dvals1);
+Evals2 = (Evals1(1:end-2)-dvals1(1:end-1).^2./d2vals1);
+
 loglog(2.^NelemValues,(abs(Evals0+.5)),'+-',...
-    (2.^NelemValues(3:end)),(abs(Evals1+.5)),'+-');
+    (2.^NelemValues(3:end)),(abs(Evals1+.5)),'+-',...
+    (2.^NelemValues(5:end)),(abs(Evals2+.5)),'+-');
 
 xlabel('number of grid points');
 ylabel('error');
