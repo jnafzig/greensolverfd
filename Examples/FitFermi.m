caeba460d640d60d703759578ae3809373b7a30b
Jonathan <jnafzig@purdue.edu>
2017-04-14 10:12:11 -0400

    cleaned up FitFermi example

--- Examples/FitFermi.m
+++ Examples/FitFermi.m
@@ -1,60 +1,25 @@
-load scan_center.mat;
+% Use the data from a run of AtomSurfacePDFTscan.m
+AtomSurfacePDFTscan_mu
 
 pdf = dNatom_dmu;
 cdf = Nrec;
 
-muvals = muspace;
+% initial guesses
+mu = -0.48;
+sigma =  5e-5;
+nu = 1;
+skew = 0;
 
-load scan_tails.mat;
-
-
-msk = muspace < -0.46 & muspace > -.6;
-
-muspace  = [muvals,muspace(msk)];
-
-pdf = [pdf;dNatom_dmu(msk)];
-cdf = [cdf;Nrec(msk)];
-
-[muspace,ix] = sort(muspace);
-pdf = pdf(ix);
-cdf = cdf(ix);
-
-
-
-
-logit = @(x) log(x)-log(1-x);
-
-% mu = muspace(find(cdf == 1, 1, 'first'))-sum(cdf(cdf<1))*dmu;
-% mu = sum(diff([cdf]).*(muspace(1:end-1)'+muspace(2:end)')/2);
-% var = sum(diff([0;cdf]).*(muspace'-mu).^2);
-
-
-mu = -0.484350161528228;
-sigma =  0.000058395539667;
-nu = 1.004848813108375;
-
-% student = truncate(makedist('tlocationscale'),lb,ub);
-student = makedist('tlocationscale');
-student.mu = mu;
-student.sigma =  sigma;
-student.nu = nu;
-
+% get the handle for the fit function
 logtfit = logtfit_fcn();
 
-x = lsqcurvefit(logtfit,[student.mu,student.sigma,student.nu,0],...
-    muspace',log(pdf),[-1,0,0,-.1],[0,1,5,.1]);
-% x = [-0.484350161528228   0.000058395539667   1.004848813108375];
-
-% student.mu = 0;
-% alpha = .001;
-% 
-% plot(muspace,[log(pdf),log(2*student.pdf(muspace'-mu).*student.cdf(alpha*(muspace'-mu)))]);
+% fit the curve
+x = lsqcurvefit(logtfit,[mu,sigma,nu,skew],...
+    muspace',log(pdf),[-1,0,0,-1],[0,1,5,1]);
 
 plot(muspace,[log(pdf),logtfit(x,muspace')]);
-% xlim([-.58,-.42]);
 
-% plot(muspace,logit([cdf,student.cdf(muspace')]));
-% xlim([-.485,-.4838]);
-
-% sqrt(var)
-% student.std
+mu = x(1)
+sigma =  x(2)
+nu = x(3)
+skew = x(4)
--- Examples/logtfit_fcn.m
+++ Examples/logtfit_fcn.m
@@ -1,19 +1,20 @@
 function [ logtfit_fcn ] = logtfit_fcn( ~ )
-    %LOSS Summary of this function goes here
-    %   Detailed explanation goes here
+    % Return a fit function for lsqcurvefit
+    % in particular it returns the log of a skewed student's t-distribution
     
-%     
-%     lb = -1;
-%     ub = -0.42;
-% 
-%     student = truncate(makedist('tlocationscale'),lb,ub);
     student = makedist('tlocationscale');
+    
+    % Distribution is centered on 0 and shifted manually
     student.mu = 0;
         
-    
     logtfit_fcn = @logtfit;
     
     function logtfit = logtfit(x,xdata)
+        % x contains an array of the parameters: x -> [mu,sigma,nu,skew]
+        % logtfit will return the log of the skewed student's
+        % t-distribution with parameters contained in x, at the values
+        % contained in xdata.
+        
         student.sigma = x(2);
         student.nu = x(3);
         
