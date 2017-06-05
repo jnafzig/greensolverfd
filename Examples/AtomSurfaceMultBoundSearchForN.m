3f8b96ad9a4f6e521fa5413616be47e81d77ab5b
Jonathan <jnafzig@purdue.edu>
2017-06-05 13:10:14 -0400

    updated search algorithm

--- Examples/AtomSurfaceMultBoundSearchForN.m
+++ Examples/AtomSurfaceMultBoundSearchForN.m
@@ -3,7 +3,7 @@ clear;
 % mu = -2.019;
 % mu = -2.010;
 % mu = -2.003;
-mu = -.5;
+mu = -1.79;
 
 L = 20; % Length of potential well
 s = 5; % steepness parameter
@@ -24,13 +24,13 @@ Nelem = max(size(x)); %Number of grid points
 
 %Initialize potential of fragment 1
 %va=-V0./(1+exp(-s*(x+R+L)))+V0./(1+exp(-s*(x+R)));
-va = -V0+V0./(1+exp(-s*(x+R)));
+v_metal = -V0+V0./(1+exp(-s*(x+R)));
 
 %initialize potential of fragment 2
-vb = -Z*cosh(x).^-2;
+v_atom = -Z*cosh(x).^-2;
 
 %Create fragment potentials & left, right BCs on fragments
-vi = [va,vb];
+vi = [v_metal,v_atom];
 vLi = [-V0,0];
 vRi = [0,0];
 
@@ -39,34 +39,37 @@ shoot = shoot_fh(Nelem,dx);
 bssolver = shootsolver_fh(Nelem,dx);
 evsolver = shooteigsolver_fh(Nelem,dx);
 
-nm = solver(mu,va+vb,sum(vLi),sum(vRi));
+nm = solver(mu,v_metal+v_atom,sum(vLi),sum(vRi));
 
-n10 = solver(mu,va,vLi(1),vRi(1));
-MaxEval = nodecount(shoot(0,vb,0,0));
-[evals,evecs] = evsolver(MaxEval,vb);
+n10 = solver(mu,v_metal,vLi(1),vRi(1));
+MaxEval = nodecount(shoot(0,v_atom,0,0));
+[evals,evecs] = evsolver(MaxEval,v_atom);
 diffs = evals - mu;
 [~,index]=min(abs(diffs));
 if index == 1
-    ncore = zeros(Nelem,1);
+    n_core = zeros(Nelem,1);
 else
-    ncore = sum(evecs(:,1:index-1).^2,2);
+    n_core = sum(evecs(:,1:index-1).^2,2);
 end
 
-nfuk = evecs(:,index).^2;
-Natom = sum((nm-n10-ncore).*nfuk)/sum(nfuk.^2);
-Natom = (index-1)+max(min(Natom,1),0);
+n_fukui = evecs(:,index).^2;
+N_atom0 = sum((nm-n10-n_core).*n_fukui)/sum(n_fukui.^2);
+N_atom0 = (index-1)+max(min(N_atom0,1),0);
+N_atom = N_atom0;
 
-n20 = bssolver(Natom,vb);
+n20 = bssolver(N_atom,v_atom);
 vp = zeros(size(nm));
 vp_pred = zeros(size(nm));
 dvp = zeros(size(nm));
 
-msk1 = nm<1e-10;
+msk1 = nm<1e-8;
 msk2 = nm>1e-15;
 
 dN = 0;
 eval = 0;
 diffmu = 1;
+N_lowbound = -inf;
+N_upbound = inf;
 iter = 0;
 maxiter = 10;
 tol = eps;
@@ -78,43 +81,71 @@ while (~done && iter <= maxiter)
     vp_pred(msk1) = vp_pred(find(~msk1,1,'last'));
 %     vp = invert({solver,bssolver},nm,[mu,Natom],vi,vLi,vRi,eps,vp_pred);
 
-    vp_part = partialinvert({solver,bssolver},nm,[mu,Natom],vi,vLi,vRi,msk2,eps,vp_pred);
+    vp_part = partialinvert({solver,bssolver},nm,[mu,N_atom],vi,vLi,vRi,msk2,eps,vp_pred);
     vp(msk2) = vp_part;
     vp(~msk2) = vp_pred(find(~msk1,1,'last'));
     
-    [na,chia] = solver(mu,va+vp,vLi(1),vRi(1));
-    [nb,chib] = bssolver(Natom,vb+vp);
-    [eval,evec] = evsolver(Natom,vb+vp);
-    fb = evec(:,end).^2;
-    nf = na+nb;
+    [n_metal,chi_metal] = solver(mu,v_metal+vp,vLi(1),vRi(1));
+    [n_atom,chi_atom] = bssolver(N_atom,v_atom+vp);
+    [eval,evec] = evsolver(N_atom,v_atom+vp);
+    f_atom = evec(:,end).^2;
+    nf = n_metal+n_atom;
  
     % differential for how vp changes for small changes in Natom with
     % total density fixed
-    dvp_dNatom = -(chib+real(chia))\fb; 
+    dvp_dNatom = -(chi_atom+real(chi_metal))\f_atom; 
     % corresponding change in atom's chemical potential
-    dmuatom_dN = (sum(dvp_dNatom.*fb)*dx);
+    dmuatom_dN = (sum(dvp_dNatom.*f_atom)*dx);
 
-    diffmu = mu-eval(ceil(Natom));
+    mu_atom = eval(ceil(N_atom));
+    
+    diffmu = mu-mu_atom;
 
     if (abs(diffmu) < tol)
         done = true;
     end
 
-    dN = diffmu/dmuatom_dN;
+    dN = diffmu/dmuatom_dN; 
 
-    if mod(Natom,1)==0
-        moddiff = 0;
+    % update bounds on N
+    if dN > 0
+        N_lowbound = N_atom;
+    
+        % should we worry about crossing integer or bound on N?
+        if N_upbound <= floor(N_atom) + 1
+           if dN + N_atom >= N_upbound
+               % then we bisect:
+               dN = (N_upbound + N_atom)/2 - N_atom;
+           end
+        else
+            if dN + N_atom >= floor(N_atom) + 1
+               % then we go to upperbounding integer
+               dN = floor(N_atom) + 1 - N_atom;
+            end
+        end
     else
-        moddiff=1-mod(Natom,1);
-    end
-    dN = min(max(-Natom,dN),moddiff);  % bound dN so N stays between integer numbers
+        N_upbound = N_atom;
 
-    % if N is at the edge of the interval and doesn't go anywhere we are
-    % done
-    if ((mod(Natom,1)==0) && dN == 0)
+        % should we worry about crossing integer or bound on N?
+        if N_lowbound >= ceil(N_atom) - 1
+           if dN + N_atom <= N_lowbound
+               % then we bisect:
+               dN = (N_lowbound + N_atom)/2 - N_atom;
+           end
+        else
+            if dN + N_atom <= ceil(N_atom) - 1
+               % then we go to lowbounding integer
+               dN = ceil(N_atom) - 1 - N_atom;
+            end
+        end
+    end
+    
+    
+    % if upper and lower bounds are equal we are done
+    if (N_lowbound == N_upbound)
         done = true;
     else
-        Natom = Natom + dN;
+        N_atom = N_atom + dN;
     end
 
     dvp = dvp_dNatom*dN;
